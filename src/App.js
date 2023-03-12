34dcf6e665fcf5f089bf1108c54327b8cff9db7c
Kyle Freemantle <kcfreemantle@gmail.com>
2023-03-12 14:24:01 -0700

    huge failure to connect front and back end again

--- src/App.js
+++ src/App.js
@@ -34,15 +34,22 @@ class App extends React.Component {
     e.preventDefault();
     try {
       let LocationIQToken = process.env.REACT_APP_LOCATIONIQ_API_KEY;
+      let cityData = await axios.get(`https://us1.locationiq.com/v1/search?key=${LocationIQToken}&q=${this.state.cityName}&format=json`);
+      let movieData = await axios.get(`${process.env.REACT_APP_SERVER}/movie?keyword=${this.state.cityName}`);
+      let weatherData = await axios.get(`${process.env.REACT_APP_SERVER}/weather?search=${this.state.cityName}&lat=${this.state.cityData.lat}&lon=${this.state.cityData.lon}`);
 
       // Axios action happens here
-      let myData = await axios.get(`https://us1.locationiq.com/v1/search?key=${LocationIQToken}&q=${this.state.cityName}&format=json`)
       this.setState({
-        cityData: myData.data[0],
+        cityData: cityData.data[0],
         latitude: this.state.cityData.lat,
         longitude: this.state.cityData.lon,
-        isMapOpen: true
-      }, this.handleWeather)
+        weatherData: weatherData.data,
+        movieData: movieData.data,
+        isMapOpen: true,
+        error: false,
+
+      }, // this.handleWeather
+      )
     } catch (error) {
       this.setState({
         error: true,
@@ -60,15 +67,15 @@ class App extends React.Component {
       })
   }
 
-  handleWeather = async () => {
-    let weatherURL = `http://localhost:3001/weather?searchQuery=${this.state.cityName}`
-    console.log(weatherURL);
-    let weatherData = await axios.get(weatherURL);
-    this.setState({
-      weatherDataState: weatherData
-    })
-    console.log(weatherData);
-  }
+  // handleWeather = async () => {
+  //   let weatherURL = `http://localhost:3001/weather?searchQuery=${this.state.cityName}`
+  //   console.log(weatherURL);
+  //   let weatherData = await axios.get(weatherURL);
+  //   this.setState({
+  //     weatherDataState: weatherData
+  //   })
+  //   console.log(weatherData);
+  // }
 
 
   render() {
@@ -81,6 +88,8 @@ class App extends React.Component {
           <Form.Control id="cityName" type="text" onChange={this.handleChange}></Form.Control>
           <Button type='submit'>Explore!</Button>
         </Form>
+
+        
 
         {this.state.weatherDataState
           &&
